656db91a214ba6a0af9619e40d760300fbe5ab4a
Michael Marini <angrymarini@gmail.com>
2017-06-04 11:22:45 +1000

    More unit tests for combinations of bundles

--- spec/lib/order_line_spec.rb
+++ spec/lib/order_line_spec.rb
@@ -15,77 +15,138 @@ describe OrderLine do
 
   describe '#fill' do
 
-    it 'calculates the order where it is a single of 9' do
-      amount, order_lines = OrderLine.fill(9, bundles)
-      expect(order_lines.size).to eql(1)
-      expect(amount).to eql(0)
+    describe 'orders of a single bundle' do
+      it 'calculates the order where it is a single of 9' do
+        amount, order_lines = OrderLine.fill(9, bundles)
+        expect(order_lines.size).to eql(1)
+        expect(amount).to eql(0)
 
-      order_line = order_lines[0]
-      expect(order_line.amount).to eql(1)
-      expect(order_line.bundle).to eql(bundle_9)
+        order_line = order_lines[0]
+        expect(order_line.amount).to eql(1)
+        expect(order_line.bundle).to eql(bundle_9)
+      end
+
+      it 'calculates the order where it is a multiple of 9' do
+        amount, order_lines = OrderLine.fill(9 * 3, bundles)
+        expect(order_lines.size).to eql(1)
+        expect(amount).to eql(0)
+
+        order_line = order_lines[0]
+        expect(order_line.amount).to eql(3)
+        expect(order_line.bundle).to eql(bundle_9)
+      end
+
+      it 'calculates the order where it is a single of 5' do
+        amount, order_lines = OrderLine.fill(5, bundles)
+        expect(order_lines.size).to eql(1)
+        expect(amount).to eql(0)
+
+        order_line = order_lines[0]
+        expect(order_line.amount).to eql(1)
+        expect(order_line.bundle).to eql(bundle_5)
+      end
+
+      it 'calculates the order where it is a multiple of 5' do
+        amount, order_lines = OrderLine.fill(5 * 2, bundles)
+
+        expect(order_lines.size).to eql(1)
+        expect(amount).to eql(0)
+
+        order_line = order_lines[0]
+        expect(order_line.amount).to eql(2)
+        expect(order_line.bundle).to eql(bundle_5)
+      end
+
+      it 'calculates the order where it is a single of 3' do
+        amount, order_lines = OrderLine.fill(3, bundles)
+
+        expect(order_lines.size).to eql(1)
+        expect(amount).to eql(0)
+
+        order_line = order_lines[0]
+        expect(order_line.amount).to eql(1)
+        expect(order_line.bundle).to eql(bundle_3)
+      end
+
+      it 'calculates the order where it is a multiple of 3' do
+        amount, order_lines = OrderLine.fill(3 * 2, bundles)
+
+        expect(order_lines.size).to eql(1)
+        expect(amount).to eql(0)
+
+        order_line = order_lines[0]
+        expect(order_line.amount).to eql(2)
+        expect(order_line.bundle).to eql(bundle_3)
+      end
+
+      it 'does not calculate if the order cannot be filled' do
+        amount, order_lines = OrderLine.fill(2, bundles)
+
+        expect(order_lines.size).to eql(0)
+        expect(amount).to eql(2)
+      end
     end
 
-    it 'calculates the order where it is a multiple of 9' do
-      amount, order_lines = OrderLine.fill(9 * 3, bundles)
-      expect(order_lines.size).to eql(1)
-      expect(amount).to eql(0)
+    describe 'orders in combinations' do
+      it 'calculates the order where it contains 1 bundle of 9, 2 bundles of 5, 1 bundle of 3' do
+        amount, order_lines = OrderLine.fill(9 + (2*5) + 3, bundles)
+        expect(order_lines.size).to eql(3)
+        expect(amount).to eq(0)
 
-      order_line = order_lines[0]
-      expect(order_line.amount).to eql(3)
-      expect(order_line.bundle).to eql(bundle_9)
+        order_line = order_lines[0]
+        expect(order_line.amount).to eql(1)
+        expect(order_line.bundle).to eql(bundle_9)
+
+        order_line = order_lines[1]
+        expect(order_line.amount).to eql(2)
+        expect(order_line.bundle).to eql(bundle_5)
+
+        order_line = order_lines[2]
+        expect(order_line.amount).to eql(1)
+        expect(order_line.bundle).to eql(bundle_3)
+      end
+
+      it 'calculates the order where it contains 1 bundle of 9, 2 bundles of 5' do
+        amount, order_lines = OrderLine.fill(9 + (2*5), bundles)
+        expect(order_lines.size).to eql(2)
+        expect(amount).to eq(0)
+
+        order_line = order_lines[0]
+        expect(order_line.amount).to eql(1)
+        expect(order_line.bundle).to eql(bundle_9)
+
+        order_line = order_lines[1]
+        expect(order_line.amount).to eql(2)
+        expect(order_line.bundle).to eql(bundle_5)
+      end
+
+      it 'calculates the order where it contains 1 bundle of 9, 2 bundles of 3' do
+        amount, order_lines = OrderLine.fill(9 + (2*3), bundles)
+        expect(order_lines.size).to eql(2)
+        expect(amount).to eq(0)
+
+        order_line = order_lines[0]
+        expect(order_line.amount).to eql(1)
+        expect(order_line.bundle).to eql(bundle_9)
+
+        order_line = order_lines[1]
+        expect(order_line.amount).to eql(2)
+        expect(order_line.bundle).to eql(bundle_3)
+      end
+
+      it 'calculates the order where it contains 2 bundle of 5, 2 bundles of 3' do
+        amount, order_lines = OrderLine.fill((2*5) + (2*3), bundles)
+        expect(order_lines.size).to eql(2)
+        expect(amount).to eq(0)
+
+        order_line = order_lines[0]
+        expect(order_line.amount).to eql(2)
+        expect(order_line.bundle).to eql(bundle_5)
+
+        order_line = order_lines[1]
+        expect(order_line.amount).to eql(2)
+        expect(order_line.bundle).to eql(bundle_3)
+      end
     end
-
-    it 'calculates the order where it is a single of 5' do
-      amount, order_lines = OrderLine.fill(5, bundles)
-      expect(order_lines.size).to eql(1)
-      expect(amount).to eql(0)
-
-      order_line = order_lines[0]
-      expect(order_line.amount).to eql(1)
-      expect(order_line.bundle).to eql(bundle_5)
-    end
-
-    it 'calculates the order where it is a multiple of 5' do
-      amount, order_lines = OrderLine.fill(5 * 2, bundles)
-
-      expect(order_lines.size).to eql(1)
-      expect(amount).to eql(0)
-
-      order_line = order_lines[0]
-      expect(order_line.amount).to eql(2)
-      expect(order_line.bundle).to eql(bundle_5)
-    end
-
-    it 'calculates the order where it is a single of 3' do
-      amount, order_lines = OrderLine.fill(3, bundles)
-
-      expect(order_lines.size).to eql(1)
-      expect(amount).to eql(0)
-
-      order_line = order_lines[0]
-      expect(order_line.amount).to eql(1)
-      expect(order_line.bundle).to eql(bundle_3)
-    end
-
-    it 'calculates the order where it is a multiple of 3' do
-      amount, order_lines = OrderLine.fill(3 * 2, bundles)
-
-      expect(order_lines.size).to eql(1)
-      expect(amount).to eql(0)
-
-      order_line = order_lines[0]
-      expect(order_line.amount).to eql(2)
-      expect(order_line.bundle).to eql(bundle_3)
-    end
-
-    it 'does not calculate if the order cannot be filled' do
-      amount, order_lines = OrderLine.fill(2, bundles)
-
-      expect(order_lines.size).to eql(0)
-      expect(amount).to eql(2)
-    end
-
   end
-
-
 end
